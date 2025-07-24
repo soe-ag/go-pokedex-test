ed43f2edc2213d816c25ca886aeedd6bd529f1c9
soeaung94 <aung.soe@triple-four.com>
2025-07-24 10:09:22 +0900

    first-repl-to-stdout

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main (){
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		if !scanner.Scan () {
+			break
+		}
+
+		input := scanner.Text()
+		words := cleanInput(input)
+
+		if len(words) == 0 {
+			continue
+		}
+
+		fmt.Printf("Your command was: %s\n", strings.ToLower(words[0]))
+	}
 }
 
 func cleanInput(text string) []string{
